c6d156c5e25b21d5fcb15b9acd707905a7085323
LeoLewandowski <leo.lewandowski@student.junia.com>
2024-01-10 19:20:55 +0100

    Quick update

---
+++ commands/help.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'), fs = require('fs');
+
+const cmdNames = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js')).map(f => {return {name:f.replace('.js', ''), value: f.replace('.js', '')}});
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('To get more informations on the commands')
+        .setDMPermission(true)
+        .addSubcommand(s =>
+            s.setName('general').setDescription('Displays all commands and their description')
+            )
+        .addSubcommand(s =>
+            s.setName('command').setDescription('Gives additionnal info on a particular command')
+            .addStringOption(o =>
+                o.setName('name').setDescription('The command\'s name').setRequired(true)
+                .addChoices(...cmdNames)
+                )
+            )
+        ,
+    async execute(interaction, Dsc, client) {
+        await interaction.deferReply();
+        if(interaction.options.getSubcommand() === 'command'){
+            const cmd = require(`./${interaction.options.getString('nom')}`)
+            if(!cmd) return interaction.editReply({ content: `Sorry, the command ${interaction.options.getString('name')} is invalid`})
+            return await interaction.editReply({ embeds: [{
+                title: `Commande ${cmd.data.name}`,
+                description: cmd.data.description,
+                color: client.color,
+                author: {
+                    name: 'Help - Command',
+                    icon_url: client.user.avatarURL(),
+                }
+            }], ephemeral: true });
+        }
+        await interaction.editReply({ content: `General help`, ephemeral: true });
+    }
+}
